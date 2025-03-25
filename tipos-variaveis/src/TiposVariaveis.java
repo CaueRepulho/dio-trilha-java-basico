57664cd68ded52178167e12b96d2b1eb94571f2f
CaueRepulho <caueferreira0@gmail.com>
2025-03-25 19:17:43 -0300

    Primeiras variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,21 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double salaraioMinimo = 2500;
+        System.out.println("Seu Salário é: " + salaraioMinimo);
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 1;
+
+        numero = 2; //a variavel pode receber outro valor desde que o tipo esteja correto
+
+        final double valorDePi = 3.14;
+
+        System.out.println(valorDePi);
+
+        String meuNome = "Caue"; //essa e a forma de criacao de uma string
+
     }
 }
